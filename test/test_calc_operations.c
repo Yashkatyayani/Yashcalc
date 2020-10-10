1509849745819f1596fc78ab666707fe3abe032b
Yashkatyayani <69120883+Yashkatyayani@users.noreply.github.com>
2020-10-10 12:29:45 +0530

    Update test_calc_operations.c

--- test/test_calc_operations.c
+++ test/test_calc_operations.c
@@ -3,7 +3,7 @@
 
 /* Modify these two lines according to the project */
 #include <calcoperator.h>
-#define PROJECT_NAME    "Calculator"
+#define PROJECT_NAME    "Yashcalc"
 
 /* Prototypes for all the test functions */
 void test_add(void);
